14973485cb4be52a11a5faa40ddab1198dde12f7
wenhaowuengeo <wwu@engeo.com>
2024-02-29 14:41:07 -0800

    reduce the interval time for the wait job func

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
-
-var htmlStr = '';
+var htmlStr = "";
 //onclick event
 
-function runFunc () {
+function runFunc() {
   //import Geoprocessor from "@arcgis/core/rest/geoprocessor";
   console.log("run button is clicked");
 
   //trigger GP service
-  require(["esri/tasks/Geoprocessor","esri/rest/support/JobInfo"], (Geoprocessor, JobInfo) => {
+  require(["esri/tasks/Geoprocessor", "esri/rest/support/JobInfo"], (
+    Geoprocessor,
+    JobInfo
+  ) => {
     // console.log("before gp def");
-    var gp = new Geoprocessor('https://mygis.engeo.com/server/rest/services/CAF_23777/siteacessemailto/GPServer/siteacessemailto/');
+    var gp = new Geoprocessor(
+      "https://mygis.engeo.com/server/rest/services/CAF_23777/siteacessemailto/GPServer/siteacessemailto/"
+    );
 
     //define the input parameters
     // var params = {
     //   "Input_Feature": "",
     // };
-  
+
     //submit the gp service
     // console.log("before gp submission");
 
-    gp.submitJob().then(function(jobInfo){
-    // gp.execute().then(function(jobInfo){ //CANNOT use execute() since the GP service job itself is defined as submitJob() operation
-      // const jobid = jobInfo.jobId;
-      // console.log("job id: ", jobid);
+    gp.submitJob()
+      .then(function (jobInfo) {
+        // gp.execute().then(function(jobInfo){ //CANNOT use execute() since the GP service job itself is defined as submitJob() operation
+        // const jobid = jobInfo.jobId;
+        // console.log("job id: ", jobid);
 
-      // const status = jobInfo.jobStatus;
-      // console.log("job status:", status);
+        const options = {
+          interval: 100, //wait for 0.1 sec
+          statusCallback: (j) => {
+            console.log("Job Status: ", j.jobStatus);
+          },
+        };
 
-      const options = {
-        interval: 1000,
-        statusCallback: (j) => {
-          console.log("Job Status: ", j.jobStatus);
-        }
-      };
-
-      jobInfo.waitForJobCompletion(options).then(() => {
-        jobInfo.fetchResultData('Send_Email').then(function(result){
-          console.log("job result:", result.value);
-          htmlStr = result.value;
-          // window.open(outputFileUrl, "_blank");
+        jobInfo.waitForJobCompletion(options).then(() => {
+          jobInfo.fetchResultData("Send_Email").then(function (result) {
+            console.log("job result:", result.value);
+            htmlStr = result.value;
+          });
         });
+      })
+      .catch(function (e) {
+        console.log("GP job failed", e);
       });
-
-    }).catch(function(e) {
-      console.log("GP job failed", e);
-    });
     console.log("GP service job submitted");
   });
-
-};
-
+}
 
 var emailBtnCounter = 0;
 
 function emailFunc() {
-  emailBtnCounter ++;
+  emailBtnCounter++;
   console.log("email button is clicked");
   // document.getElementById("emailButtonID").style.color = 'blue';
 
-  var emailDiv = document.createElement('div');
+  var emailDiv = document.createElement("div");
   emailDiv.style.textAlign = "center";
 
   if (htmlStr) {
@@ -67,9 +66,6 @@ function emailFunc() {
 
   if (emailBtnCounter == 1) {
     // document.getElementsByTagName('body')[0].appendChild(document.createElement('br'))
-    document.getElementsByTagName('body')[0].appendChild(emailDiv);
+    document.getElementsByTagName("body")[0].appendChild(emailDiv);
   }
-  
-};
-
-
+}
